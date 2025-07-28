0c7fc7e8fbcb6bd7fac6ffefc0ed38fccafd70f6
Prateek Verma <prateekv700@gmail.com>
2025-07-28 21:24:41 +0530

    Time: 43 ms (28.8%), Space: 14.1 MB (78.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,11 +5,11 @@ public:
         for(int i=0;i<nums.size();i++){
            for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
-                    return {i,j};
+                    return result = {i,j};
                     
                 }
            }
         }
-        return{};
+        return result;
     }
 };
