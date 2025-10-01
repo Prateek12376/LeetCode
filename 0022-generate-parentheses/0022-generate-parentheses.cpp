c1b67b123fd662ab34f0ad6150539a8babe40810
Prateek Verma <prateekv700@gmail.com>
2025-10-01 18:36:33 +0530

    Time: 3 ms (71.06%), Space: 13 MB (88.29%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void find_comb(int oBracket, int cBracket, int count_bracket, string& s, vector<string>& ans){
+        if(oBracket==cBracket && (oBracket+cBracket)==2*count_bracket){
+            ans.push_back(s);
+            return;
+        }
+        if(oBracket<count_bracket){
+            s.push_back('(');
+            find_comb(oBracket+1,cBracket,count_bracket,s,ans);
+            s.pop_back();
+        }
+        if(cBracket<oBracket){
+            s.push_back(')');
+            find_comb(oBracket,cBracket+1,count_bracket,s,ans);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string s;
+        find_comb(0,0,n,s,ans);
+        return ans;
+    }
+};
