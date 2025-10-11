d9aef40522bf4d021184ac0eb7be06d42ee807d3
Prateek Verma <prateekv700@gmail.com>
2025-10-11 20:14:09 +0530

    Time: 39 ms (59.9%), Space: 62.7 MB (52.67%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    void find_comb(int index,vector<int>& arr,vector<vector<int>>& ans,int k,int n){
-        if(arr.size()==k){
+    void find_comb(int index,vector<int>& arr,vector<vector<int>>& ans,int k,vector<int>& nums){
+        if(index>=nums.size()){
+            if(arr.size()==k){
             ans.push_back(arr);
+            }
             return;
         }
-        for(int i=index;i<=n;i++){
-            arr.push_back(i);
-            find_comb(i+1,arr,ans,k,n);
-            arr.pop_back();
-        }
+        arr.push_back(nums[index]);
+        find_comb(index+1,arr,ans,k,nums);
+        arr.pop_back();
+        find_comb(index+1,arr,ans,k,nums);
+
+        
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int>arr;
         vector<vector<int>>ans;
-        find_comb(1,arr,ans,k,n);
+        vector<int>nums;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+        }
+        find_comb(0,arr,ans,k,nums);
         return ans;
     }
 };
