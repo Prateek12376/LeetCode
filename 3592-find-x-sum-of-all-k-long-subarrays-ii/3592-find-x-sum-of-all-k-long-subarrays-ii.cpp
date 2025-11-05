8b06ab71cbe1d5aa929ebec113543a1026a94f00
Prateek Verma <prateekv700@gmail.com>
2025-11-05 16:32:10 +0530

    Time: 728 ms (63.46%), Space: 280.5 MB (84.62%) - LeetHub

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/3592-find-x-sum-of-all-k-long-subarrays-ii.cpp
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/3592-find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -1,70 +1,65 @@
 class Solution {
 public:
-    void insertInSet(const pair<int,int>& p, long long& sum, set<pair<int,int>>& main,set<pair<int,int>>& sec,int x){
-    if(main.size()<x || p> *main.begin()){
-        sum+=1LL*p.first* p.second ;
-        main.insert(p);
-        if(main.size()>x){
-        auto smallest= *main.begin();
-        sum-= 1LL* smallest.first* smallest.second;
-        main.erase(smallest);
-        sec.insert(smallest);
-        }
-    }
-    else{
-        sec.insert(p);
-    }
-    }
-    void removeFromSet(const pair<int,int>& p, long long& sum, set<pair<int,int>>& main,set<pair<int,int>>& sec,int x){
-    if(main.find(p)!=main.end()){
-        sum-=1LL*p.first*p.second;
-        main.erase(p);
-        if(!sec.empty()){
-        auto largest = * sec.rbegin();
-        sec.erase(largest);
-        main.insert(largest);
-        sum+=1LL*largest.first*largest.second;
-        }
-    }
-    else{
-        sec.erase(p);
-    }
-    }
-
-    vector<long long> findXSum(vector<int>& nums, int k, int x){
-    int n=nums.size();
-    long long sum=0;
-    vector<long long> res;
-    set<pair<int,int>>main;
-    set<pair<int,int>>sec;
-
-    unordered_map<int,int>mp;  // {freq->nums}
-
-    int i=0;
-    int j=0;
-    while(j<n){
-        if(mp[nums[j]]>0){
-        removeFromSet({mp[nums[j]],nums[j]},sum,main,sec,x);
-        }
-        mp[nums[j]]++;
-
-        insertInSet({mp[nums[j]],nums[j]},sum,main,sec,x);
-
-        if(j-i+1==k){
-        res.push_back(sum);
-
-        removeFromSet({mp[nums[i]],nums[i]},sum,main,sec,x);
-        mp[nums[i]]--;
-        if(mp[nums[i]]==0){
-            mp.erase(nums[i]);
+    void removeFromSet(const pair<int,int>& p,set<pair<int,int>>& sec, set<pair<int,int>>& main, long long& sum, int x){
+        if(main.find(p)!=main.end()){
+            sum-=1LL*p.first*p.second;
+            main.erase(p);
+            if(!sec.empty()){
+                auto largest= *sec.rbegin();
+                sec.erase(largest);
+                main.insert(largest);
+                sum+=1LL*largest.first*largest.second;
+            }
         }
         else{
-            insertInSet({mp[nums[i]],nums[i]},sum,main,sec,x);
+            sec.erase(p);
         }
-        i++;
-        }
-        j++;
     }
-    return res;
+    void insertInSet(const pair<int,int>& p,set<pair<int,int>>& sec, set<pair<int,int>>& main, long long& sum, int x){
+        if(main.size()<x || p> *main.begin()){
+            sum+=1LL*p.first*p.second;
+            main.insert(p);
+            if(main.size()>x){
+                auto smallest= *main.begin();
+                sum-=1LL* smallest.first*smallest.second;
+                main.erase(smallest);
+                sec.insert(smallest);
+            }
+        }
+        else{
+            sec.insert(p);
+        }
+    }
+    vector<long long> findXSum(vector<int>& nums, int k, int x) {
+        vector<long long>res;
+        long long sum=0;
+        set<pair<int,int>>main;  // top elm
+        set<pair<int,int>>sec;    // rem elm
+        unordered_map<int,int>freq;
+
+        int i=0;
+        int j=0;
+        while(j<nums.size()){
+            if(freq[nums[j]]>0){
+                removeFromSet({freq[nums[j]],nums[j]},sec,main,sum,x);
+            }
+            freq[nums[j]]++;
+            insertInSet({freq[nums[j]],nums[j]},sec,main,sum,x);
+
+            if(j-i+1==k){
+                res.push_back(sum);
+                removeFromSet({freq[nums[i]],nums[i]},sec,main,sum,x);
+                freq[nums[i]]--;
+                if(freq[nums[i]]==0){
+                    freq.erase(nums[i]);
+                }
+                else{
+                    insertInSet({freq[nums[i]],nums[i]},sec,main,sum,x);
+                }
+                i++;
+            }
+            j++;
+        }
+        return res;
     }
 };
