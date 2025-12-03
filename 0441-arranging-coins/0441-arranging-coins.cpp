2c7bd7f5ab785bbe69dd10ff8e52d65c7da886ae
Prateek Verma <prateekv700@gmail.com>
2025-12-03 20:40:39 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.74%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int low=0;
+        int high=n;
+
+        while(low<=high){
+            long long mid =  1LL*low+(high-low)/2;
+
+            if(n>=mid*(mid+1)/2){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low-1;
+    }
+};
