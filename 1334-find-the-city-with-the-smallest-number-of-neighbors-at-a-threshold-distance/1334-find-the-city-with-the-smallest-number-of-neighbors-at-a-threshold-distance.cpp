db279a43f67d8cd5c909b6b13505558b0185da77
Prateek Verma <prateekv700@gmail.com>
2026-01-24 16:59:35 +0530

    Time: 27 ms (18.66%), Space: 19.5 MB (23.54%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<pair<int,int>>>adj(n);
+        vector<int>neighbour(n,0);
+
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int w=edges[i][2];
+
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        int src=n-1;
+        while(src>=0){
+            int count=0;
+            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;  //{weight,node}
+            vector<int>dist(n,INT_MAX);
+            dist[src]=0;
+            pq.push({0,src});
+
+            while(!pq.empty()){
+                auto it = pq.top();
+                pq.pop();
+                int cost= it.first;
+                int node =it.second;
+                for(auto it: adj[node]){
+                    int adjN=it.first;
+                    int adjC=it.second;
+                    if(adjC+cost<dist[adjN] && adjC+cost<=distanceThreshold){
+                        dist[adjN]=adjC+cost;
+                        pq.push({dist[adjN],adjN});
+                    }
+                }
+            }
+            for(int i=0;i<n;i++){
+                if(dist[i]!=INT_MAX && dist[i]<=distanceThreshold && dist[i]!=0){
+                    count++;
+                }
+            }
+            neighbour[src]=count;
+            src--;
+        }
+        int ans=neighbour[0];
+        int ans2=0;
+        for(int i=1;i<n;i++){
+            if(neighbour[i]<=ans){
+                ans=neighbour[i];
+                ans2=i;
+            }
+        }
+        return ans2;
+    }
+};
