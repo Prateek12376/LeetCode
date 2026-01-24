2dfc50e1b5427f3a1584524cd92addcc0ff1419f
Prateek Verma <prateekv700@gmail.com>
2026-01-24 10:36:42 +0530

    Time: 187 ms (8.21%), Space: 100 MB (30.18%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -3,11 +3,12 @@ public:
     int minPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        priority_queue<int,vector<int>,less<int>>pq;
-        for(int i=0;i<n;i++){
+        int ans=0;
+        for(int i=0;i<n/2;i++){
             int sum=nums[i]+nums[n-i-1];
-            pq.push(sum);
+            ans=max(ans,sum);
+            
         }
-        return pq.top();
+        return ans;
     }
 };
