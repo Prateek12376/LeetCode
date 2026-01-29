a2a440ff0d43aef36fffbac63e9f5d3b7d71b28f
Prateek Verma <prateekv700@gmail.com>
2026-01-29 18:39:28 +0530

    Time: 59 ms (33.49%), Space: 121.5 MB (13.4%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost){
-        vector<vector<long long>>dist(26,vector<long long>(26,LLONG_MAX));
-
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<pair<int,long long>>>adj(26);
         for(int i=0;i<original.size();i++){
-            int u= original[i]-'a';
+            int u = original[i]-'a';
             int v= changed[i]-'a';
-            long long wt = 1LL* cost[i];
-            dist[u][v]=min(dist[u][v],wt);
+            long long wt =cost[i];
+            adj[u].push_back({v,wt});
         }
+        vector<vector<long long>>c_frm_node(26,vector<long long>(26,LLONG_MAX));
         for(int i=0;i<26;i++){
-            dist[i][i]=0;
-        }
+            int src=i;
+            priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
+            vector<long long>dist(26,LLONG_MAX);
+            dist[src]=0;
+            pq.push({0,src});
+            while(!pq.empty()){
+                auto it = pq.top();
+                pq.pop();
+                int node = it.second;
+                long long wt = it.first;
 
-        for(int k=0;k<26;k++){
-            for(int i=0;i<26;i++){
-            for(int j=0;j<26;j++){
-                if(dist[i][k]<LLONG_MAX && dist[k][j]<LLONG_MAX){
-                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                for(auto it : adj[node]){
+                    int  adjN=it.first;
+                    long long wtN = it.second;
+                    if(wt+wtN<dist[adjN]){
+                        dist[adjN]=wt+wtN;
+                        pq.push({dist[adjN],adjN});
+                    }
                 }
             }
-            }
+            c_frm_node[i]=dist;
         }
         long long min_cost=0;
         for(int i=0;i<source.size();i++){
             int from = source[i]-'a';
-            int to= target[i]-'a';
-            if(dist[from][to]==LLONG_MAX){
-            return -1;
+            int to = target[i]-'a';
+
+            if(c_frm_node[from][to]==LLONG_MAX){
+                return -1;
             }
-            min_cost+=dist[from][to];
+            min_cost+=c_frm_node[from][to];
         }
         return min_cost;
+
+        
     }
 };
