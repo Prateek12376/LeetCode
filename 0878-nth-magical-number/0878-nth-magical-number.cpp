aa064bcfc998e7d1187c1e036341392733fc57b1
Prateek Verma <prateekv700@gmail.com>
2026-02-19 09:31:38 +0530

    Time: 0 ms (100%), Space: 7.8 MB (42.87%) - LeetHub

---
+++ 0878-nth-magical-number/0878-nth-magical-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        long mod = 1e9 + 7;
+        long low = 1;
+        long long high = (long)n * min(a, b);
+        
+        long gcd = __gcd(a, b);
+        long lcm = (long)a * b / gcd;
+        
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+            
+            long count = mid/a + mid/b - mid/lcm;
+            
+            if (count < n)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        
+        return low % mod;
+    }
+};
