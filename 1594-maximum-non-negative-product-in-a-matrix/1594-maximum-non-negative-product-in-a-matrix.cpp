104e523284d0dc01e89216317e92d44417381532
Prateek Verma <prateekv700@gmail.com>
2026-03-23 17:31:17 +0530

    Time: 0 ms (100%), Space: 14 MB (87.22%) - LeetHub

---
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    pair<long long ,long long> find_maxP(int i,int j,vector<vector<int>>& grid,vector<vector<pair<long long,long long>>>& dpp){
+        int mod=1e9+7;
+        if(i==0 && j==0){
+            return {grid[i][j],grid[i][j]};
+        }
+        if(dpp[i][j].first!=LLONG_MIN){
+            return dpp[i][j];
+        }
+        // left
+        long long maxV  = LLONG_MIN;
+        long long minV= LLONG_MAX;
+        if(i>0){
+            auto[leftMax, leftMin]=find_maxP(i-1,j,grid,dpp);
+            maxV= max(maxV,max(grid[i][j]*leftMax,grid[i][j]*leftMin));
+            minV= min(minV,min(grid[i][j]*leftMax,grid[i][j]*leftMin));
+        }
+
+        // up
+        if(j>0){
+            auto[upMax,upMin]= find_maxP(i,j-1,grid,dpp);
+            maxV=max(maxV,max(grid[i][j]*upMax,grid[i][j]*upMin));
+            minV=min(minV,min(grid[i][j]*upMax,grid[i][j]*upMin));
+
+        }
+        return dpp[i][j]={maxV,minV};
+
+    }
+    int maxProductPath(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+
+        vector<vector<pair<long long ,long long>>>dpp(n,vector<pair<long long,long long>>(m,{LLONG_MIN,LLONG_MAX}));
+        pair<long long,long long> result=  find_maxP(n-1,m-1,grid,dpp);
+        if(result.first<0){
+            return -1;
+        }
+        long long mod=(1e9+7);
+        return  (int)(result.first%mod);
+        
+    }
+};
