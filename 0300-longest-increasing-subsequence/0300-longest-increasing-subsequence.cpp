e56f1e6c4c7a22d1bbd747900aab20b2d7b3eba9
Prateek Verma <prateekv700@gmail.com>
2026-03-25 22:02:48 +0530

    Time: 670 ms (5.05%), Space: 296.4 MB (18.41%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,8 +4,8 @@ public:
         if(ind==nums.size()){
             return 0;
         }
-        if(dpp[ind+1][prevI+1]!=-1){
-            return dpp[ind+1][prevI+1];
+        if(dpp[ind][prevI+1]!=-1){
+            return dpp[ind][prevI+1];
         }
         int pick=0;
         int n_pick=0;
@@ -24,12 +24,12 @@ public:
             //not pick
             n_pick=0+find_length(ind+1,nums,prevI,dpp);
         }
-        return dpp[ind+1][prevI+1]=max(pick,n_pick);
+        return dpp[ind][prevI+1]=max(pick,n_pick);
         
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dpp(n+1,vector<int>(n+1,-1));
+        vector<vector<int>>dpp(n,vector<int>(n+1,-1));
         return find_length(0,nums,-1,dpp);
     }
 };
