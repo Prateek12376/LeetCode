6f2f455d0da4f48ff8b22cf4d91d29c9f62b1d78
Prateek Verma <prateekv700@gmail.com>
2026-03-25 21:56:28 +0530

    Time: 723 ms (5.05%), Space: 296.5 MB (11.66%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int find_length(int ind, vector<int>& nums, int prevI,vector<vector<int>>& dpp){
+        if(ind==nums.size()){
+            return 0;
+        }
+        if(dpp[ind+1][prevI+1]!=-1){
+            return dpp[ind+1][prevI+1];
+        }
+        int pick=0;
+        int n_pick=0;
+        if(prevI==-1){
+            // pick
+            pick=1+find_length(ind+1,nums,ind,dpp);
+            //not pick
+            n_pick=find_length(ind+1,nums,prevI,dpp);
+
+        }
+        else{
+            // pick
+            if(nums[prevI]<nums[ind]){
+                pick=1+find_length(ind+1,nums,ind,dpp);
+            }
+            //not pick
+            n_pick=0+find_length(ind+1,nums,prevI,dpp);
+        }
+        return dpp[ind+1][prevI+1]=max(pick,n_pick);
+        
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dpp(n+1,vector<int>(n+1,-1));
+        return find_length(0,nums,-1,dpp);
+    }
+};
