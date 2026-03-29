2495cb73188b37b20c36bfdbe8d352ed2faea3f2
Prateek Verma <prateekv700@gmail.com>
2026-03-29 12:30:01 +0530

    Time: 0 ms (100%), Space: 8.2 MB (67.1%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        int n=s1.size();
+        for(int i=n-1;i>=2;i--){
+            if(s1[i]!=s2[i]){
+                if( s1[i-2]!=s2[i]){
+                    return false;
+                }
+                else{
+                    swap(s1[i],s1[i-2]);
+                }
+            }
+        }
+        for(int i=0;i<=1;i++){
+            if(s1[i]!=s2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
