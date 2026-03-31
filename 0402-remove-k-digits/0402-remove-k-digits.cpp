113ea19c5444616299b26ee70a68e0b2bbc91d46
Prateek Verma <prateekv700@gmail.com>
2026-03-31 15:19:48 +0530

    Time: 12 ms (7.27%), Space: 11.7 MB (30.47%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -4,33 +4,34 @@ public:
         int n=num.size();
         stack<char>st;
         for(int i=0;i<n;i++){
-            while(!st.empty() && (num[i]-'0'<st.top()-'0')&& k>0){
+            while(!st.empty() && k>0 && num[i]<st.top()){
                 st.pop();
                 k--;
             }
             st.push(num[i]);
         }
-        while(k>0){
+        while(k>0 && !st.empty()){
             st.pop();
             k--;
         }
         if(st.empty()){
             return "0";
         }
-        string  result="";
+        string ans="";
         while(!st.empty()){
-            result+=st.top();
+            ans+=st.top();
             st.pop();
         }
-        while(result.size()!=0 && result.back()=='0'){
-            result.pop_back();
+        reverse(ans.begin(),ans.end());
+        int ind3=0;
+        while(ind3<ans.size() && ans[ind3]=='0'){
+            ind3++;
         }
-        reverse(result.begin(),result.end());
-        if(result.size()==0){
+        ans= ans.substr(ind3);
+        if(ans.size()==0){
             return "0";
         }
-        else{
-            return result;
-        }
+
+        return ans;
     }
 };
