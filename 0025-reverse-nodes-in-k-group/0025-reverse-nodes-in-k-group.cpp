37c29d5bae7470a4787f654730bfaf62da958418
Prateek Verma <prateekv700@gmail.com>
2026-04-09 22:40:08 +0530

    Time: 0 ms (100%), Space: 16.6 MB (38.26%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,47 +10,48 @@
  */
 class Solution {
 public:
-    ListNode* find_kth_node(ListNode* temp,int k){
+    ListNode* find_kth(ListNode* head, int k){
         k=k-1;
-        while(temp!=NULL && k>0){
+        while(head!=NULL && k>0){
             k--;
-            temp=temp->next;
+            head=head->next;
         }
-        return temp;
+        return head;
     }
-    ListNode* reverse_kth_node(ListNode* head){
+    ListNode* reverseN(ListNode* head){
         if(head==NULL || head->next==NULL){
             return head;
         }
-        ListNode* new_head= reverse_kth_node(head->next);
-        ListNode* front= head->next;
+        ListNode* reverseH  = reverseN(head->next);
+        ListNode* front = head->next;
         front->next=head;
         head->next=NULL;
-        return new_head;
+        return reverseH;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* temp=head;
-        ListNode* prev_Last=NULL;
-        ListNode* nextNode;
-        while(temp!=NULL){
-            ListNode* kth_Node= find_kth_node(temp,k);
-            if(kth_Node==NULL){
-                if(prev_Last!=NULL){
-                    prev_Last->next=temp;
+       ListNode* temp=head;
+       ListNode* prevN=NULL;
+       ListNode* nextN;
+       while(temp!=NULL){
+            ListNode* kth_node =  find_kth(temp,k);
+            if(kth_node==NULL){
+                if(prevN!=NULL){
+                    prevN->next=temp;
                 }
                 break;
             }
-            nextNode=kth_Node->next;
-            kth_Node->next=NULL;
-            ListNode* rev_head= reverse_kth_node(temp);
+            nextN=kth_node->next;
+            kth_node->next=NULL;
+            ListNode* reverseH= reverseN(temp);
+
             if(temp==head){
-                head=rev_head;
+                head=reverseH;
             }
             else{
-                prev_Last->next=rev_head;
+                prevN->next=reverseH;
             }
-            prev_Last=temp;
-            temp=nextNode;
+            prevN=temp;
+            temp=nextN;
         }
         return head;
     }
