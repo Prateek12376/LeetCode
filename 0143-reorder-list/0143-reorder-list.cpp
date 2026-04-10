58f5474639a00a694d424ae6768b479997ef1582
Prateek Verma <prateekv700@gmail.com>
2026-04-10 22:01:48 +0530

    Time: 3 ms (12.29%), Space: 23.6 MB (17.65%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -24,35 +24,24 @@ public:
         if(head==NULL || head->next==NULL){
             return;
         }
-        ListNode* newH= new ListNode(head->val);
-        ListNode* curr = head->next;
-        ListNode* temp1= newH;
-        int count=1;
-        while(curr!=NULL){
-            count++;
-            ListNode* n1 = new ListNode(curr->val);
-            temp1->next=n1;
-            temp1=temp1->next;
-            curr=curr->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        ListNode* revH = reverseLL(newH);
-        ListNode* temp = head;
-        ListNode* revt = revH;
-        int lim=(count)/2;
-        while(lim>0){
-            ListNode* nextNode = temp->next;
-            ListNode* n2 = new ListNode(revt->val);
-            revt = revt->next;
-            temp->next = n2;
-            n2->next = nextNode;
-            temp = nextNode;
-            lim--;
+        ListNode* revH=reverseLL(slow->next);
+        slow->next=NULL;
+        ListNode* first=head;
+        ListNode* second = revH;
+
+        while(second!=NULL){
+            ListNode* temp1= first->next;
+            ListNode* temp2= second->next;
+            first->next=second;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
         }
-        ListNode* t = head;
-        while(t!=NULL && count>1){
-            count--;
-            t=t->next;
-        }
-        t->next=NULL;
     }
 };
