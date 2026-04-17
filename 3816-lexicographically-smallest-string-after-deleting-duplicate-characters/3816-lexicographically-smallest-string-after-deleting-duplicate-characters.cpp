6f0d23b65ae2ed333d162cfecb5d353e85e65097
Prateek Verma <prateekv700@gmail.com>
2026-04-17 18:42:08 +0530

    Time: 125 ms (29.35%), Space: 36.7 MB (19.79%) - LeetHub

---
+++ 3816-lexicographically-smallest-string-after-deleting-duplicate-characters/3816-lexicographically-smallest-string-after-deleting-duplicate-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string lexSmallestAfterDeletion(string s) {
+        vector<int>freq(26,0);
+        vector<int>rem(26,0);
+        for(int i=0;i<s.size();i++){
+            rem[s[i]-'a']++;
+        }
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+            rem[s[i]-'a']--;
+            while(!st.empty() && st.top()>s[i] && (freq[st.top()-'a']+ rem[st.top()-'a'])>1){
+                freq[st.top()-'a']--;
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+        string ans ="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        int j=ans.size()-1;
+        while(j>0 && (ans[j]<=ans[j-1] || ans[j]>=ans[j-1] )&& freq[ans[j]-'a']>1){
+            freq[ans[j]-'a']--;
+            j--;
+        }
+        string ans1="";
+        for(int i=0;i<=j;i++){
+            ans1+=ans[i];
+        }
+        return ans1;
+    }
+};
