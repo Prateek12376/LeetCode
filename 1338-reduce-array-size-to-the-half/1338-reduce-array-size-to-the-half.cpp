c57984480bbc0a87e96b6b7165bf3e9977fd8c52
Prateek Verma <prateekv700@gmail.com>
2026-04-21 19:01:56 +0530

    Time: 91 ms (33.44%), Space: 85.5 MB (16.44%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int>mpp;
+        vector<int>nums;
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+        for(auto it: mpp){
+            nums.push_back(it.second);
+        }
+        sort(nums.rbegin(),nums.rend());
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            count++;
+            if(sum>=(arr.size()/2)){
+                return count;
+            }
+
+        }
+        return {};
+    }
+};
