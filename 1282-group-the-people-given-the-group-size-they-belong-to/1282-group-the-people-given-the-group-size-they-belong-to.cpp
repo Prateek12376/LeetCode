418b754171770f0a939f1f85b48c41ae062f8b17
Prateek Verma <prateekv700@gmail.com>
2026-04-21 18:54:03 +0530

    Time: 4 ms (51.12%), Space: 18.2 MB (13.5%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -6,9 +6,9 @@ public:
         for(int i=0;i<groupSizes.size();i++){
             mpp[groupSizes[i]].push_back(i);
         }
-        for(auto& it: mpp){
+        for(auto it: mpp){
             int size=it.first;
-            vector<int>ans=it.second;
+            vector<int>& ans=it.second;
             int i=0;
             while(i<ans.size()){
                 vector<int>arr;
