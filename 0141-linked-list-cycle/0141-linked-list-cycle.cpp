195bad50afe39ddc7327fb9511109b7f80322278
Prateek Verma <prateekv700@gmail.com>
2026-04-23 23:56:19 +0530

    Time: 9 ms (54.56%), Space: 11.9 MB (21.99%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,11 +12,11 @@ public:
         if(head==NULL || head->next==NULL){
             return false;
         }
+        ListNode* slow= head;
         ListNode* fast=head;
-        ListNode* slow=head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
+        while(fast->next!=NULL && fast->next->next!=NULL){
             slow=slow->next;
+            fast=fast->next->next;
             if(slow==fast){
                 return true;
             }
