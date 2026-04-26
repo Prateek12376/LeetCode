84c43e7c3098eda94a4f0d2ba6a14df9a007780a
Prateek Verma <prateekv700@gmail.com>
2026-04-26 15:30:31 +0530

    Time: 501 ms (11.91%), Space: 132.6 MB (30.99%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool dfs(int r,int c, vector<vector<char>>& grid,vector<vector<int>>& vis,int tr, int tc){
+        vis[r][c]=1;
+        vector<pair<int,int>>dir={{-1,0},{1,0},{0,-1},{0,1}};
+
+        for(auto it : dir){
+            int nr= r+it.first;
+            int nc=c+it.second;
+            if(nr>=0 && nc>=0 && nr<grid.size() && nc<grid[0].size()){
+                if(grid[nr][nc]!=grid[r][c]){
+                    continue;
+                }
+                if(vis[nr][nc]==0){
+                    if(dfs(nr,nc,grid,vis,r,c)==true){
+                        return true;
+                    }
+                }
+                else if(vis[nr][nc]==1){
+                    if(nr!=tr || nc!=tc){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+    bool containsCycle(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0){
+                    if(dfs(i,j,grid,vis,-1,-1)==true){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
