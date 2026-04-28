0071e8aa5355f98e17ea2456c642e8a8a243a55b
Prateek Verma <prateekv700@gmail.com>
2026-04-28 23:32:21 +0530

    Time: 19 ms (76.52%), Space: 62.5 MB (41.7%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool hasValidPath(vector<vector<int>>& grid) {
+        vector<vector<pair<int,int>>>dir(7);
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        dir[1]={{0,-1},{0,1}};
+        dir[2]={{-1,0},{1,0}};
+        dir[3]={{0,-1},{1,0}};
+        dir[4]={{0,1},{1,0}};
+        dir[5]={{0,-1},{-1,0}};
+        dir[6]={{0,1},{-1,0}};
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vis[0][0]=1;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int r=it.first;
+            int c= it.second;
+            if(r==n-1 && c==m-1){
+                return true;
+            }
+            for(auto it : dir[grid[r][c]]){
+                int nr= r+ it.first;
+                int nc=c+it.second;
+
+                if(nr<n && nr>=0 && nc>=0 && nc<m && vis[nr][nc]==0){
+                    for(auto it : dir[grid[nr][nc]]){
+                        int nrB=nr+it.first;
+                        int ncB=nc+it.second;
+                        if(nrB==r && ncB==c){
+                            vis[nr][nc]=1;
+                            q.push({nr,nc});
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
