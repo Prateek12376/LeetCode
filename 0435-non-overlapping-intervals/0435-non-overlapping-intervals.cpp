e34f6b2dc1466d8e5af274bccdc6b54267c7db9b
Prateek Verma <prateekv700@gmail.com>
2026-05-01 21:08:34 +0530

    Time: 2202 ms (5.03%), Space: 511.2 MB (5.15%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,28 +1,23 @@
-struct Data{
-    int start;
-    int end;
-};
-static bool comp(Data a, Data b){
-    return a.end<b.end;
-}
+
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int n=intervals.size();
-        vector<Data>interval_arr(n);
-        for(int i=0;i<n;i++){
-            interval_arr[i]={intervals[i][0],intervals[i][1]};
-        }
-        sort(interval_arr.begin(),interval_arr.end(),comp);
-        int count=1;
-        int Last_endTime= interval_arr[0].end;
+        
+        sort(intervals.begin(),intervals.end(),[&](vector<int>a, vector<int>b){
+            return a[1]<b[1];
+        });
+        vector<vector<int>>ans;
+        int count=0;
+        ans.push_back(intervals[0]);
         for(int i=1;i<n;i++){
-            if(interval_arr[i].start>=Last_endTime){
+            if(intervals[i][0]<ans.back()[1]){
                 count++;
-                Last_endTime= interval_arr[i].end;
+            }
+            else{
+                ans.push_back(intervals[i]);
             }
         }
-        int remove= n-count;
-        return remove;
+        return count;
     }
 };
