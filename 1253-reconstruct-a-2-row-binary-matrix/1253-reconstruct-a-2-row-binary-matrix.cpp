c8a2385b9bc0bd25c49e489f02b3b132d955feed
Prateek Verma <prateekv700@gmail.com>
2026-05-02 17:14:34 +0530

    Time: 3 ms (73.08%), Space: 65.2 MB (39.42%) - LeetHub

--- 1253-reconstruct-a-2-row-binary-matrix/1253-reconstruct-a-2-row-binary-matrix.cpp
+++ 1253-reconstruct-a-2-row-binary-matrix/1253-reconstruct-a-2-row-binary-matrix.cpp
@@ -7,31 +7,22 @@ public:
             if(colsum[i]==2){
                 ans[0][i]=1;
                 upper--;
-                colsum[i]--;
                 ans[1][i]=1;
                 lower--;
-                colsum[i]--;
             }
             else if(colsum[i]==1){
                 if(lower>upper){
                     ans[1][i]=1;
                     lower--;
-                    colsum[i]--;
                 }
                 else{
                     ans[0][i]=1;
                     upper--;
-                    colsum[i]--;
                 }
             }
         }
         if(lower!=0 || upper!=0){
             return {};
-        }
-        for(int i=0;i<n;i++){
-            if(colsum[i]>0){
-                return {};
-            }
         }
         return ans;
     }
