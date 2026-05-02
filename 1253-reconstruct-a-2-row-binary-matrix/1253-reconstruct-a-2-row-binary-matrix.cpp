bac7e59013a1abe31c79c7050443921715fcf496
Prateek Verma <prateekv700@gmail.com>
2026-05-02 17:09:22 +0530

    Time: 5 ms (48.56%), Space: 65.2 MB (39.42%) - LeetHub

---
+++ 1253-reconstruct-a-2-row-binary-matrix/1253-reconstruct-a-2-row-binary-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructMatrix(int upper, int lower, vector<int>& colsum) {
+        int n=colsum.size();
+        vector<vector<int>>ans(2,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            if(colsum[i]==2){
+                ans[0][i]=1;
+                upper--;
+                colsum[i]--;
+                ans[1][i]=1;
+                lower--;
+                colsum[i]--;
+            }
+            else if(colsum[i]==1){
+                if(lower>upper){
+                    ans[1][i]=1;
+                    lower--;
+                }
+                else{
+                    ans[0][i]=1;
+                    upper--;
+                }
+            }
+        }
+        if(lower!=0 || upper!=0){
+            return {};
+        }
+        return ans;
+    }
+};
