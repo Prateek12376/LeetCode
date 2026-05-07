8386b3e433c9e692cbc70a2a11b592ccd2e06dad
Prateek Verma <prateekv700@gmail.com>
2026-05-07 21:43:20 +0530

    Time: 16 ms (43.09%), Space: 225.8 MB (34.04%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.cpp
+++ 3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -15,15 +15,15 @@ public:
         int mini=nums[n-1];
         vector<int>ans(n,0);
         for(int i=n-1;i>=0;i--){
-            if(prefM[i]>suffMin){
-                if(i+1<n){
+            if(prefM[i]>suffMin){   // once we enter the suffMin region , then in the end we can reach the max elm 
+                if(i+1<n){          // we can jump forward , if saller present after 
                     ans[i]=ans[i+1];
                 }
                 else{
                     ans[i]=prefM[i];
                 }
             }
-            else{
+            else{                // other wise conider max till that elm
                 ans[i]=prefM[i];
             }
             suffMin=min(suffMin,nums[i]);
