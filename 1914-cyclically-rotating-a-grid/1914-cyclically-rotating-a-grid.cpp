0f0a124fff802f276266edc64220e82d57b7b28b
Prateek Verma <prateekv700@gmail.com>
2026-05-09 17:50:07 +0530

    Time: 8 ms (92.65%), Space: 17.9 MB (34.56%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(int start,vector<vector<int>>& vis, vector<vector<int>>& ans,vector<int>& arr,int k){
+    void dfs(int start,vector<vector<int>>& ans,vector<int>& arr,int k){
         int size= arr.size();
         int n=ans.size();
         int m=ans[0].size();
@@ -30,30 +30,22 @@ public:
     vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
         vector<vector<int>>ans=grid;
         for(int i=0;i<min(n,m)/2;i++){
-            if(vis[i][i]==0){
-                vis[i][i]=1;
-                vector<int>arr;
-                for(int row=i;row<n-i;row++){
-                    arr.push_back(grid[row][i]);
-                    vis[row][i]=1;
-                }
-                for(int col=i+1;col<m-i;col++){
-                    arr.push_back(grid[n-i-1][col]);
-                    vis[n-i-1][col]=1;
-                }
-                for(int row=n-i-2;row>=0+i;row--){
-                    arr.push_back(grid[row][m-i-1]);
-                    vis[row][m-i-1]=1;
-                }
-                for(int col=m-i-2;col>=1+i;col--){
-                    arr.push_back(grid[i][col]);
-                    vis[i][col]=1;
-                }
-                dfs(i,vis,ans,arr,k);
+            vector<int>arr;
+            for(int row=i;row<n-i;row++){
+                arr.push_back(grid[row][i]);
             }
+            for(int col=i+1;col<m-i;col++){
+                arr.push_back(grid[n-i-1][col]);
+            }
+            for(int row=n-i-2;row>=0+i;row--){
+                arr.push_back(grid[row][m-i-1]);
+            }
+            for(int col=m-i-2;col>=1+i;col--){
+                arr.push_back(grid[i][col]);
+            }
+            dfs(i,ans,arr,k);
         }
         return ans;
     }
