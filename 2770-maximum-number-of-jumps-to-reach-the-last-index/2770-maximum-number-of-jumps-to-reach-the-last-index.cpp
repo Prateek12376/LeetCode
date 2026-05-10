3d0ecb335b6ca2576722862e63fc6c4870189df3
Prateek Verma <prateekv700@gmail.com>
2026-05-10 12:15:49 +0530

    Time: 20 ms (54.04%), Space: 68.6 MB (19.88%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int maxJ(int ind, int jump, vector<int>& nums,int target, vector<vector<int>>& dpp){
+    int maxJ(int ind,vector<int>& nums,int target, vector<int>& dpp){
         if(ind==nums.size()-1){
             return 0;
         }
-        if(dpp[ind][jump]!=-1){
-            return dpp[ind][jump];
+        if(dpp[ind]!=-1){
+            return dpp[ind];
         }
         int maxi=-1e5;
         for(int i=ind+1;i<nums.size();i++){
             if(abs(nums[ind]-nums[i])<=target){
-                int next=maxJ(i,jump+1,nums,target,dpp);
+                int next=maxJ(i,nums,target,dpp);
                 if(next!=-1e5){
                     maxi=max(maxi,next+1);
                 }
             }
 
         }
-        return dpp[ind][jump]=maxi;
+        return dpp[ind]=maxi;
     }
     int maximumJumps(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<vector<int>>dpp(n,vector<int>(n,-1));
-        int ans= maxJ(0,0,nums,target,dpp);
+        vector<int>dpp(n,-1);
+        int ans= maxJ(0,nums,target,dpp);
         if(ans<=0){
             return -1;
         }
