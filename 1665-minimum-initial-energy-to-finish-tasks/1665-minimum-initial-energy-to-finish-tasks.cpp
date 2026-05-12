0ad46fa78cd4194115aec57bba0f16ce57980028
Prateek Verma <prateekv700@gmail.com>
2026-05-12 13:48:22 +0530

    Time: 1760 ms (7.77%), Space: 487.1 MB (6.31%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
         sort(tasks.begin(),tasks.end(),[&](vector<int>a, vector<int>b){
-            // if((a[1]-a[0])==(b[1]-b[0])){
-            //     return a[1]>b[1];
-            // }
             return (a[1]-a[0])>(b[1]-b[0]);
-
         });
         int count=tasks[0][1];
         int rem= count-tasks[0][0];
