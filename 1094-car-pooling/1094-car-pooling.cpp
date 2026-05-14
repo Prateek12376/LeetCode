79badb2c97bb4ca7d8bff5f9c6c6ae715986f2b9
Prateek Verma <prateekv700@gmail.com>
2026-05-14 22:01:25 +0530

    Time: 0 ms (100%), Space: 14.4 MB (51.51%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int>diff(1001,0);
+        vector<int>dropArr(1001,0);
         for(int i=0;i<trips.size();i++){
             int cap=trips[i][0];
             int src=trips[i][1];
             int dest=trips[i][2];
-            diff[src]+=cap;
-            diff[dest]-=cap;
+            dropArr[src]+=cap;
+            dropArr[dest]-=cap;
         }
         int curr=0;
         for(int i=0;i<1001;i++){
-            curr+=diff[i];
+            curr+=dropArr[i];
             if(curr>capacity){
                 return false;
             }
