50b7715dcf77b0e142b690b350e2278d07f17ba2
Prateek Verma <prateekv700@gmail.com>
2026-05-27 10:03:06 +0530

    Time: 129 ms (14.04%), Space: 26.2 MB (9.83%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
         unordered_map<char,int>lower;
-        unordered_map<char,int>upper;
         unordered_map<char,int>current;
         vector<int>vis(26,0);
 
         for(int i=0;i<word.size();i++){
             if(islower(word[i])){
                 lower[word[i]]++;
-            }
-            else{
-                upper[word[i]]++;
             }
         }
         for(int i=0;i<word.size();i++){
