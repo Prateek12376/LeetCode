c4df7115eda3deb8ef83831a2ada9bf7aeea1e05
Prateek Verma <prateekv700@gmail.com>
2026-06-06 14:02:38 +0530

    Time: 0 ms (100%), Space: 15 MB (63.06%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -5,21 +5,19 @@ public:
         if(n==1){
             return {0};
         }
-        vector<int>leftS(n);
-        vector<int>rightS(n);
+        int Tsum=0;
+        for(int i=0;i<n;i++){
+            Tsum+=nums[i];
+        }
         vector<int>ans(n);
-        leftS[0]=nums[0];
-        for(int i=1;i<n;i++){
-            leftS[i]=nums[i]+leftS[i-1];
-        }
-        rightS[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            rightS[i]=nums[i]+rightS[i+1];
-        }
-        ans[0]=rightS[1];
-        ans[n-1]=leftS[n-2];
-        for(int i=1;i<n-1;i++){
-            ans[i]=abs(leftS[i-1]-rightS[i+1]);
+
+        int leftS=0;
+        for(int i=0;i<n;i++){
+            Tsum-=nums[i];
+            int rightS= Tsum;
+
+            ans[i]=abs(leftS-rightS);
+            leftS+= nums[i];
         }
         return ans;
 
