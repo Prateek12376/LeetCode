5d21a3677d85fdfd08f9e56d810c19b9554f6e47
Prateek Verma <prateekv700@gmail.com>
2026-06-11 10:15:51 +0530

    Time: 295 ms (52.35%), Space: 338.4 MB (42.95%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adj, int cnt, int& maxP,vector<int>& vis){
+        vis[node]=1;
+        maxP= max(maxP,cnt);
+        for(auto it : adj[node]){
+            if(vis[it]==0){
+                dfs(it,adj,cnt+1,maxP,vis);
+            }
+        }
+
+    }
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        int mod = 1e9 +7 ;
+        int count=0;
+        int n = edges.size();
+        vector<vector<int>>adj(n+2);
+        vector<int>vis(n+2,0);
+
+        for(int i=0;i<n;i++){
+            int u=edges[i][0];
+            int v= edges[i][1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        int maxP=0;
+
+        dfs(1,adj,0,maxP,vis);
+
+        // 2^(maxP-1)
+        
+        int a = maxP-1;
+        int ans =1;
+        while(a>0){
+            ans=(ans*2)%mod;
+            a--;
+        }
+        return ans;
+        
+    }
+};
