483cdaeaa05430c9b2d90c3b5803545b21a4bcf3
Prateek Verma <prateekv700@gmail.com>
2026-06-17 22:03:14 +0530

    Time: 0 ms (100%), Space: 9.4 MB (62.43%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -8,29 +8,23 @@ public:
                 zero++;
             }
         }
-
-        vector<long long>two(33);
-        two[0]=1;
-        for(int i=1;i<33;i++){
-            two[i]= 2*two[i-1];
-        }
+        long long pow=1;
         long long num=0;
         int cnt=0;
         int n=s.size();
         for(int i=s.size()-1;i>=0;i--){
             if(s[i]=='1'){
-                int pos=n-i-1;
-                if(pos>32){
-                    break;
-                }
-                num+=two[n-i-1];
 
-                if(num<=(long long)k){
+                if((num+pow)<=(long long)k){
+                    num+=pow;
                     cnt++;
                 }
-                else{
-                    break;
-                }
+            }
+            if(pow<=(long long)k){
+                pow=pow<<1;
+            }
+            else{
+                break;
             }
         }
         return zero+cnt;
