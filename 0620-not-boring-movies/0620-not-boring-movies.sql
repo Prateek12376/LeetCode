30bcafd7affb5cee4d9cf692c484811ad215b65a
Prateek Verma <prateekv700@gmail.com>
2026-06-21 23:49:58 +0530

    Time: 326 ms (10.3%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 select * from Cinema
-where mod(id,2)=1 && description!="boring" order by(rating) desc;
+where mod(id,2)=1 and description!="boring" order by(rating) desc;
