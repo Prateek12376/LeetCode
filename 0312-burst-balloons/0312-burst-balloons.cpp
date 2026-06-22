8e1811bbb4c0581f94106131e1e7ececdeee00ce
Prateek Verma <prateekv700@gmail.com>
2026-06-22 14:38:12 +0530

    Time: 207 ms (21.47%), Space: 13.8 MB (59.05%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int find_maxC(int i, int j,vector<int>& nums, vector<vector<int>>& dpp){
+    int find_maxCost(int i, int j, vector<int>& nums , vector<vector<int>>& dpp){
         if(i>j){
             return 0;
         }
         if(dpp[i][j]!=-1){
             return dpp[i][j];
         }
-        int maxi=INT_MIN;
+        int maxC= INT_MIN;
+
         for(int k=i;k<=j;k++){
-            int cost = nums[i-1]*nums[k]*nums[j+1] + find_maxC(i,k-1,nums,dpp) + find_maxC(k+1,j,nums,dpp);
-            maxi=max(maxi,cost);
+            int cost  = nums[i-1]*nums[k]*nums[j+1] + find_maxCost(i,k-1,nums,dpp)+ find_maxCost(k+1,j,nums,dpp);
+            maxC =max(maxC,cost);
         }
-        return dpp[i][j]=maxi;
+        return dpp[i][j]=maxC;
     }
     int maxCoins(vector<int>& nums) {
-        int n=nums.size();
+        int n= nums.size();
+
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         vector<vector<int>>dpp(n+2,vector<int>(n+2,-1));
-        return find_maxC(1,n,nums,dpp);
+
+        return find_maxCost(1,n,nums,dpp);
     }
 };
