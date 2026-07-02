2ec5fc2233026b28be4a7dc2a7d10b4c456a9443
Prateek Verma <prateekv700@gmail.com>
2026-07-02 15:27:21 +0530

    Time: 182 ms (12.94%), Space: 82.2 MB (8.78%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,48 +1,50 @@
 class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        queue<pair<int,pair<int,int>>>q;
-        int n=grid.size();
-        int m=grid[0].size();
+       int n= grid.size();
+       int m= grid[0].size();
 
         if(grid[0][0]>=health){
             return false;
         }
 
+        queue<pair<int,pair<int,int>>>q;
+        vector<vector<vector<int>>>vis(n,vector<vector<int>>(m,vector<int>(health,0)));
         q.push({grid[0][0],{0,0}});
-        vector<pair<int,int>>dir= {{0,-1},{0,1},{-1,0},{1,0}};
-        vector<vector<vector<int>>>vis(n,vector<vector<int>>(m,vector<int>(health+1,0)));
+
+        vector<pair<int,int>>dir={{0,-1},{0,1},{-1,0},{1,0}};
         vis[0][0][grid[0][0]]=1;
+
         while(!q.empty()){
             auto it = q.front();
             q.pop();
-            int  h = it.first;
-            int r= it.second.first;
-            int c = it.second.second;
-
+            int r=it.second.first;
+            int c=it.second.second;
+            int h = it.first;
             if(r==n-1 && c==m-1){
                 return true;
             }
 
-
             for(auto it : dir){
                 int nr= r+it.first;
-                int nc = c+it.second;
+                int nc= c+ it.second;
 
-                if(nr<n && nc<m && nr>=0 && nc>=0 && vis[nr][nc][h]==0){
+                if(nr<n && nr>=0 && nc<m && nc>=0){
                     if(grid[nr][nc]==1){
-                        if(h+1<health && vis[nr][nc][h+1]==0){
-                            q.push({h+1,{nr,nc}});
-                            vis[nr][nc][h+1]=1;
+                        if(1+h<health && vis[nr][nc][1+h]==0){
+                            q.push({1+h,{nr,nc}});
+                            vis[nr][nc][1+h]=1;
                         }
                     }
-                    else{
-                        q.push({h,{nr,nc}});
+                    else if(vis[nr][nc][h]==0){
                         vis[nr][nc][h]=1;
+                        q.push({h,{nr,nc}});
                     }
                 }
             }
         }
         return false;
+
+
     }
 };
